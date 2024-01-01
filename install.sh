1a5072ec3db37bb42cbf9830a45dd0785da41716
yxliaoyx <yxliaoyx@gmail.com>
2024-01-01 21:59:16 +0800

    Update install.sh for Ubuntu 22.04

--- install.sh
+++ install.sh
@@ -5,16 +5,17 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-apt update
-
 echo "Install SteamCMD"
 echo steam steam/license note '' | debconf-set-selections
 echo steam steam/question select 'I AGREE' | debconf-set-selections
-apt install -y steamcmd
+add-apt-repository -y multiverse
+dpkg --add-architecture i386
+apt update
+NEEDRESTART_MODE=a apt install -y steamcmd
 
 echo "Install Project Zomboid dedicated server"
 mkdir -p /opt/pzserver
-steamcmd +runscript $PWD/update_zomboid.txt
+/usr/games/steamcmd +runscript $PWD/update_zomboid.txt
 
 echo "Enable zomboid.service"
 cp zomboid.service /usr/lib/systemd/system/zomboid.service
@@ -22,6 +23,6 @@ cp zomboid.socket /usr/lib/systemd/system/zomboid.socket
 systemctl enable zomboid.service --now
 
 echo "Set up auto shutdown script"
-apt install -y net-tools
+NEEDRESTART_MODE=a apt install -y net-tools
 cp auto_shutdown.sh /opt/auto_shutdown.sh
 cp cron /etc/cron.d/cron
